9f03f936516fe09aadd14c393ebc4cc3df083ac7
ValentinCasas <101840863+ValentinCasas@users.noreply.github.com>
2023-04-19 17:32:50 -0300

    dialogo funcional

--- app/src/main/java/usuario/example/tpanexoa/ui/salir/SalirFragment.java
+++ app/src/main/java/usuario/example/tpanexoa/ui/salir/SalirFragment.java
@@ -3,20 +3,15 @@ package usuario.example.tpanexoa.ui.salir;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.navigation.NavigationView;
-
-import usuario.example.tpanexoa.R;
 import usuario.example.tpanexoa.databinding.FragmentSalirBinding;
 
-public class SalirFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
+public class SalirFragment extends Fragment {
 
     private FragmentSalirBinding binding;
 
@@ -28,22 +23,13 @@ public class SalirFragment extends Fragment implements NavigationView.OnNavigati
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSalirBinding.inflate(inflater, container, false);
-
+        mostrarDialogoDeConfirmacion();
         //NavigationView navView = binding.getRoot().findViewById(R.id.nav_view);
         //navView.setNavigationItemSelectedListener(this);
 
         return binding.getRoot();
     }
 
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.nav_salirFragment) {
-            mostrarDialogoDeConfirmacion(); // Llamamos al método para mostrar el diálogo
-            return true; // Indicamos que la selección ha sido manejada correctamente
-        }
-        return false;
-    }
 
     private void mostrarDialogoDeConfirmacion() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
